7bc6ee5a8797e1e91335c677a24e897de9d14e75
tuda2008 <1756991323@qq.com>
2018-08-24 16:35:15 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 AdminUser.create(email: 'admin@breo.cn', password: 'password', password_confirmation: 'password')
 User.create(name: '张三', email: 'hehe@breo.cn', mobile: '13800000001', bank_name: 'sz', card: '12345678901234567890', cert_id: '211322199104035028', cert_address: 'sz')
-User.create(name: '李四', email: 'haha@breo.cn', mobile: '13800000002', bank_name: 'sz', card: '12345678901234567891', cert_id: '211322199104035029', cert_address: 'sz')
-User.create(name: '王五', email: 'test@breo.cn', mobile: '13800000003', bank_name: 'sz', card: '12345678901234567892', cert_id: '211322199104035030', cert_address: 'sz')
-StockCompany.create(name: '倍轻松', description: '按摩器材', legal_representative: '张三')
+StockCompany.create(name: '深圳市倍轻松科技股份有限公司', description: '按摩器材')
+StockCompany.create(name: '深圳市鼎元宏投资有限公司', description: '按摩器材')
+StockCompany.create(name: '深圳市倍润投资有限公司', description: '按摩器材')
